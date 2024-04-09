05a8d2114b1171eeee5944fec37d6342bbed74ff
sldjfrnfwltkdfuf <163097225+sldjfrnfwltkdfuf@users.noreply.github.com>
2024-04-09 15:31:42 +0900

    Add files via upload

---
+++ 과제.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+   int i = 0, j = 0;   
+
+   for(i = 2; i <= 10; i++){
+      for(j = 2; j < i; j++){  
+         if(i % j == 0)       
+            break;             
+      }
+      if(i == j)              
+         printf("%4d", i);     
+   }
+
+   printf("\n");
+
+   return 0;
+}
